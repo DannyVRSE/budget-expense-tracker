71b7f436abb04681f771d3cb38d084d7ec3195ca
Daniel Ouma <mshindi.daniel@outlook.com>
2024-12-07 10:18:49 +0300

    feat. (internet identity)

--- src/budget_frontend/src/App.jsx
+++ src/budget_frontend/src/App.jsx
@@ -1,30 +1,69 @@
-import { useState } from 'react';
-import { budget_backend } from 'declarations/budget_backend';
+import { useEffect, useState } from 'react';
+//import { budget_backend } from 'declarations/budget_backend';
+import { AuthClient } from "@dfinity/auth-client";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    budget_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  const [identity, setIdentity] = useState(null);
+
+  // One day in nanoseconds
+  const days = BigInt(1);
+  const hours = BigInt(24);
+  const nanoseconds = BigInt(3600000000000);
+
+  const login = async () => {
+    //create an auth client
+    const authClient = await AuthClient.create();
+    //start the login process
+    await new Promise((resolve) => {
+      authClient.login(
+        {
+          identityProvider:
+            process.env.DFX_NETWORK === "ic"
+              ? "https://identity.ic0.app/#authorize"
+              : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943#authorize`,
+          // Maximum authorization expiration is 8 days
+          maxTimeToLive: days * hours * nanoseconds,
+          onSuccess: resolve,
+        }
+      )
+    })
+
+    //get the identity
+    setIdentity(authClient.getIdentity());
+
+  };
+
+  //logout
+  const logout = async () => {
+    const authClient = await AuthClient.create();
+    if (await authClient.isAuthenticated()) {
+      console.log(`Logging out ${identity.getPrincipal().toText()}`);
+      await authClient.logout();
+    }
+    setIdentity(null);
+  };
+
+  useEffect(() => {
+    //check if the user is already authenticated
+    const checkAuthenticated = async () => {
+      const authClient = await AuthClient.create();
+      if (await authClient.isAuthenticated()) {
+        setIdentity(authClient.getIdentity());
+      }
+    };
+    checkAuthenticated();
+  }, []);
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <>
+      <h1>IC Budget</h1>
+      {!identity && <button onClick={login}>Login</button>}
+      {identity && <><p>Logged in as {identity.getPrincipal().toText()}</p>
+        <button onClick={logout}>Logout</button></>
+      }
+
+    </>
   );
 }
 
